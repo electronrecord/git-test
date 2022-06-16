6dc22f3185363544428eb188ea2cad0d4c8b6b2d
Cristian Gherghel <electronrecord@gmail.com>
2022-06-16 18:02:42 +0100

    added ev listener

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function handleSubmit (ev) {
   console.log(email.value)
   console.log(password.value)
 }
+
+// const form = document.querySelector('form')
+// form.addEventListener('submit', function () {
+//   const email = document.getElementById('email')
+//   const password = document.getElementById('password')
+//   console.log(email.value)
+//   console.log(password.value)
+// })
